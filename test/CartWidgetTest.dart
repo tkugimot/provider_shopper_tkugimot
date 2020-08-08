df411e306f2c5765e724fc5d8a105bd5f079a49e
tkugimot <toshimitsu_kugimoto@yahoo.co.jp>
2020-08-08 16:01:05 +0900

    Add widget tests

---
+++ test/CartWidgetTest.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:provider/provider.dart';
+import 'package:provider_shopper_tkugimot/main.dart';
+import 'package:provider_shopper_tkugimot/models/CatalogModel.dart';
+import 'package:provider_shopper_tkugimot/models/CartModel.dart';
+import 'package:provider_shopper_tkugimot/screens/CartScreen.dart';
+
+CartModel cartModel;
+CatalogModel catalogModel;
+Widget createCartScreen() => MultiProvider(
+  providers: [
+    Provider(create: (context) => CatalogModel()),
+    ChangeNotifierProxyProvider<CatalogModel, CartModel>(
+      create: (context) => CartModel(),
+      update: (context, catalog, cart) {
+        catalogModel = catalog;
+        cartModel = cart;
+        cart.catalog = catalogModel;
+        return cart;
+      },
+    ),
+  ],
+  child: MaterialApp(
+    home: MyCart(),
+  ),
+);
+
+void main() {
+  group('CartScreen widget tests', () {
+    testWidgets('Tapping BUY button displays snackbar.', (tester) async {
+      await tester.pumpWidget(createCartScreen());
+
+      // Verify no snackbar initially exists.
+      expect(find.byType(SnackBar), findsNothing);
+      await tester.tap(find.text('BUY'));
+      // Schedule animation.
+      await tester.pump();
+      // Verifying the snackbar upon clicking the button.
+      expect(find.byType(SnackBar), findsOneWidget);
+    });
+
+    testWidgets('Testing when the cart contains items', (tester) async {
+      await tester.pumpWidget(createCartScreen());
+
+      // Adding five items in the cart and testing.
+      for (int i = 0; i < 5; i++) {
+        var item = catalogModel.getByPosition(i);
+        cartModel.add(item);
+        await tester.pumpAndSettle();
+        expect(find.text(item.name), findsOneWidget);
+      }
+
+      // Testing total price of the five items.
+      expect(find.text('\$${42 * 5}'), findsOneWidget);
+      expect(find.byIcon(Icons.done), findsNWidgets(5));
+    });
+  });
+}
